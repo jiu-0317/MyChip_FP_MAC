0e2132959f5ac848dc2771b73a903f256391fa04
minjae Baek <alstjwo98@naver.com>
2026-06-01 18:35:57 +0900

    2

--- mant_mult_lut.v
+++ mant_mult_lut.v
@@ -3,11 +3,9 @@
 module mant_mult_lut (
     input  [2:0] mant_a,
     input  [2:0] mant_b,
-    output reg [7:0] product
+    output [7:0] product
 );
 
-    always @(*) begin
-        assign mant_product = {1'b1, mant_weight} * {1'b1, mant_input};
-    end
+    assign product = {1'b1, mant_a} * {1'b1, mant_b};
 
 endmodule
